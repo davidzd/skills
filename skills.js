d27c972361f0fedb7715db30e2e28ca45303fe06
andrienko <andrienko@my.com>
2014-03-14 12:28:00 +0200

    New version (14.03)

--- skills.js
+++ skills.js
@@ -3,41 +3,41 @@ $.fn.hasAttr = function(name) {
 };
 
 var skilltree = {
-    buttons:'',
-    hint:'',
-    size:80,
-    init:function(obj){
+    buttons: '',
+    hint: '',
+    size: 80,
+    init: function(obj) {
 
         var that = this;
 
         this.buttons = obj.find('.skill');
         this.hint = obj.find('.skillHint');
 
-        if(typeof this.hint[0]=='undefined'){
+        if(typeof this.hint[0] == 'undefined') {
             obj.append('<div class="skillHint"></div>');
-            this.hint=obj.find('.skillHint');
+            this.hint = obj.find('.skillHint');
         }
 
-        this.buttons.click(function(e){
-            if(e.button == 0){
-                if($(this).hasClass('available')){
+        this.buttons.click(function(e) {
+            if(e.button == 0) {
+                if($(this).hasClass('available')) {
 
                     var current = parseInt($(this).attr('current'));
                     var max = parseInt($(this).attr('max'));
 
-                    if(current<max){
-                        current=current+1;
-                        $(this).attr('current',current);
+                    if(current < max) {
+                        current = current + 1;
+                        $(this).attr('current', current);
                         that.renderAll();
                     }
 
-                    that.hint.find('[showlevel]').hide().filter(function(){
+                    that.hint.find('[showlevel]').hide().filter(function() {
                         var showlevel = $(this).attr('showlevel');
-                        if(showlevel.indexOf('-')!= -1){
+                        if(showlevel.indexOf('-') != -1) {
                             var levels = showlevel.split('-');
-                            if(current>=parseInt(levels[0]) && current<=parseInt(levels[1]))return true;
+                            if(current >= parseInt(levels[0]) && current <= parseInt(levels[1]))return true;
                         }
-                        else if(showlevel==current)return true;
+                        else if(showlevel == current)return true;
                         return false;
                     }).show();
 
@@ -47,38 +47,44 @@ var skilltree = {
 
         });
 
-        this.buttons.bind('contextmenu', function(e){
-            //TODO: Add level decreasing
-            console.log('Stop rightclicking me!\nJoking. Feel free to rightclick me.\nThis is a stub.');
+        this.buttons.bind('contextmenu', function(e) {
+            if(that.isDowngradePossible($(this))){
+                var current = parseInt($(this).attr('current'));
+                if(current > 0) {
+                    current = current - 1;
+                    $(this).attr('current', current);
+                    that.renderAll();
+                }
+            }
             return false;
         });
 
         // Showing and hiding the tooltip
 
         this.buttons.hover(
-            function(){
+            function() {
                 var hintDiv = $(this).find('div');
-                if(typeof hintDiv[0]!='undefined'){
+                if(typeof hintDiv[0] != 'undefined') {
                     that.hint.html(hintDiv.html());
                     var current = parseInt($(this).attr('current'));
 
                     //that.hint.find('[showlevel]').hide();
                     //that.hint.find('[showlevel='+current+']').show();
 
-                    that.hint.find('[showlevel]').hide().filter(function(){
+                    that.hint.find('[showlevel]').hide().filter(function() {
                         var showlevel = $(this).attr('showlevel');
-                        if(showlevel.indexOf('-')!= -1){
+                        if(showlevel.indexOf('-') != -1) {
                             var levels = showlevel.split('-');
-                            if(current>=parseInt(levels[0]) && current<=parseInt(levels[1]))return true;
+                            if(current >= parseInt(levels[0]) && current <= parseInt(levels[1]))return true;
                         }
-                        else if(showlevel==current)return true;
+                        else if(showlevel == current)return true;
                         return false;
                     }).show();
 
                     that.hint.show();
                 }
             },
-            function(){
+            function() {
                 that.hint.html('');
                 that.hint.hide();
 
@@ -88,10 +94,10 @@ var skilltree = {
         // TODO: Check dimensions for tooltip
         // Moving the tooltip
 
-        this.buttons.mousemove(function(e){
+        this.buttons.mousemove(function(e) {
             that.hint.css({
-                left:  e.pageX,
-                top:   e.pageY
+                left: e.pageX,
+                top: e.pageY
             });
         })
 
@@ -103,101 +109,144 @@ var skilltree = {
 
     // Getting and evauluating complex dependency for obj's level.
 
-    getDependency:function(obj,level){
+    getDependency: function(obj, level) {
         if(!obj.hasAttr('dependency'))return false;
-        try{
-            eval('var evalResult = {'+obj.attr('dependency')+'}');
+        try {
+            eval('var evalResult = {' + obj.attr('dependency') + '}');
         }
-        catch(e){
+        catch(e) {
+            obj.removeAttr('dependency');
             return false;
         }
 
-        if(typeof level != 'undefined'){
+        if(typeof level != 'undefined') {
             if(typeof evalResult[level] != 'undefined')return evalResult[level];
             else return false;
         }
         else return evalResult;
     },
 
-
     // Getting and evauluating complex dependency for obj's level.
 
-    getSprite:function(obj,level){
+    getSprite: function(obj, level) {
         if(!obj.hasAttr('sprites'))return false;
-        try{
-            eval('var evalResult = {'+obj.attr('sprites')+'}');
+        try {
+            eval('var evalResult = {' + obj.attr('sprites') + '}');
         }
-        catch(e){
+        catch(e) {
+            console.log('Error in evaluating',obj.attr('sprites'));
             return false;
         }
 
-        if(typeof level != 'undefined'){
+        if(typeof level != 'undefined') {
             if(typeof evalResult[level] != 'undefined')return evalResult[level];
             else return false;
         }
         else return false;
     },
 
-
     // Checking, if upgrade of obj to level forLevel is possible
 
-    isDependencyMet:function(obj,forLevel){
-        var dep = this.getDependency(obj,forLevel);
+    isDependencyMet: function(obj, forLevel) {
+        var dep = this.getDependency(obj, forLevel);
         var mustHave = obj.attr('musthave');
 
-        if(dep!=false){
-            var dependencymet=true;
-            for(var name in dep){
-                var lvl = parseInt($('[skillid='+name+']').attr('current'));
-                if(isNaN(lvl) || lvl<0 || lvl<parseInt(dep[name])){
+        if(dep != false) {
+            var dependencymet = true;
+            for(var name in dep) {
+                var lvl = parseInt($('[skillid=' + name + ']').attr('current'));
+                if(isNaN(lvl) || lvl < 0 || lvl < parseInt(dep[name])) {
                     dependencymet = false;
                 }
             }
             return dependencymet;
         }
-        else if(typeof mustHave=='undefined' || typeof $('[skillid='+mustHave+'].active')[0] !='undefined'){
+        else if(typeof mustHave == 'undefined' || typeof $('[skillid=' + mustHave + '].active')[0] != 'undefined') {
             return true;
         }
         else return false;
     },
 
+    // Checking if downgrade is possible
+
+    isDowngradePossible: function(obj) {
+
+        // TODO: Rewrite this.. to make it work
+
+        var levelFrom = parseInt(obj.attr('current'));
+        if(isNaN(levelFrom) || levelFrom<=0)return false;
+
+        var id =  obj.attr('skillid');
+
+        if(typeof id == 'undefined')return true;   // Always possible for skills with no id
+
+        if(levelFrom == 1 && typeof this.buttons.filter('.active[musthave="'+id+'"]')[0] == 'undefined'){
+            return true;
+        }
+
+        var isPossible = true;
+        var that = this;
+        this.buttons.filter('.active[dependency]').each(function(){
+            var dep = that.getDependency($(this));
+            var current = parseInt($(this).attr('current'));
+            if(dep != false){
+                for(var lvl in dep){
+                    if(parseInt(lvl)<=current){
+                        if(typeof dep[lvl][id]!='undefined' && dep[lvl][id] >=levelFrom){
+                            isPossible = false;
+                            return false
+                        }
+                    }
+                }
+            }
+        });
+        return isPossible;
+    },
+
     // Checking things and updating single skill (obj)
 
-    render:function(obj){
+    render: function(obj) {
 
         // Getting current and max numbers
 
-        var current =   parseInt(obj.attr('current'));
-        if(isNaN(current) || current<0){current=0;obj.attr('current',0);}
+        var current = parseInt(obj.attr('current'));
+        if(isNaN(current) || current < 0) {
+            current = 0;
+            obj.attr('current', 0);
+        }
         var max = parseInt(obj.attr('max'));
-        if(isNaN(max) || current < 0){max=1;obj.attr('max',1);}
+        if(isNaN(max) || current < 0) {
+            max = 1;
+            obj.attr('max', 1);
+        }
 
         // Adding status display div
 
         var status = obj.find('.status');
-        if(typeof status[0]=='undefined'){
+        if(typeof status[0] == 'undefined') {
             obj.append('<span class="status"></span>');
             status = obj.find('.status');
         }
-        status.html(current+'/'+max);
+        status.html(current + '/' + max);
 
         // Modifying the sprite if any
 
-        var sprite = this.getSprite(obj,current);
+        var sprite = this.getSprite(obj, current);
 
-        if(sprite!=false)obj.css('background-position','-'+(parseInt(sprite[0])*this.size)+'px -'+(parseInt(sprite[1])*this.size)+'px');
+        if(sprite != false)obj.css('background-position', '-' + (parseInt(sprite[0]) * this.size) + 'px -' + (parseInt(sprite[1]) * this.size) + 'px');
 
         // Making already upgraded active
 
-        if(current>0)obj.addClass('active');
+        if(current > 0)obj.addClass('active');
+        else obj.removeClass('active');
 
         // Checking if upgrade to next level is possible
 
-        if(current<max){
-            if(this.isDependencyMet(obj,current+1))obj.addClass('available');
+        if(current < max) {
+            if(this.isDependencyMet(obj, current + 1))obj.addClass('available');
             else obj.removeClass('available');
         }
-        else if(current>=max)obj.removeClass('available');
+        else if(current >= max)obj.removeClass('available');
 
         return this;
 
@@ -205,10 +254,10 @@ var skilltree = {
 
     // Checking stuff and rendering all elements
 
-    renderAll:function(){
+    renderAll: function() {
         var that = this;
         this.buttons.each(
-            function(){
+            function() {
                 that.render($(this));
             }
         );
@@ -216,7 +265,7 @@ var skilltree = {
     }
 };
 
-$(function(){
+$(function() {
 
     skilltree.init($('.skilltree')).renderAll();
 
